dc7308376586d68da3408ba8a2fd6b696bc3afd4
Seth Schroeder <seth.schroeder@gmail.com>
2023-06-12 16:58:23 -0400

    More flexibility in repo detection

---
+++ README.md
@@ -0,0 +1,21 @@
+# CredoCheckErrorHandlingEctoOban
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `credo_check_error_handling_ecto_oban` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:credo_check_error_handling_ecto_oban, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at <https://hexdocs.pm/credo_check_error_handling_ecto_oban>.
+
---
+++ lib/credo_check_error_handling_ecto_oban/check/transaction_error_in_oban_job.ex
@@ -0,0 +1,194 @@
+defmodule CredoCheckErrorHandlingEctoOban.Check.TransactionErrorInObanJob do
+  @moduledoc """
+  Repo.transaction will return a 4 tuple when an error occurs inside a Multi.
+  Verify that errors are being mapped to a 2 tuple that Oban will interpret as expected.
+
+  iex(14)> [warning] Expected Elixir.MyApp.MultiFailure.perform/1 to return:
+  - `:ok`
+  - `:discard`
+  - `{:ok, value}`
+  - `{:error, reason}`,
+  - `{:cancel, reason}`
+  - `{:discard, reason}`
+  - `{:snooze, seconds}`
+  Instead received:
+  {:error, :alas, :poor_yorick, %{}}
+
+  The job will be considered a success.
+  """
+
+  use Credo.Check,
+    base_priority: :normal,
+    category: :warning,
+    explanations: [
+      check: """
+      Repo.transaction will return a 4 tuple when an error occurs inside a Multi.
+      Verify that errors are being mapped to a 2 tuple that Oban will interpret as expected.
+
+      iex(14)> [warning] Expected Elixir.MyApp.MultiFailure.perform/1 to return:
+      - `:ok`
+      - `:discard`
+      - `{:ok, value}`
+      - `{:error, reason}`,
+      - `{:cancel, reason}`
+      - `{:discard, reason}`
+      - `{:snooze, seconds}`
+      Instead received:
+      {:error, :alas, :poor_yorick, %{}}
+
+      The job will be considered a success.
+      """
+    ]
+
+  def run(%SourceFile{} = source_file, params \\ []) do
+    issue_meta = IssueMeta.for(source_file, params)
+
+    Credo.Code.prewalk(source_file, &traverse(&1, &2, nil, issue_meta))
+  end
+
+  # Simplify the code by looking only at relevant files
+  defp traverse({:defmodule, _, body} = ast, issues, _, _) do
+    if in_header?(body, :use, :Oban) do
+      {ast, issues}
+    else
+      {{}, issues}
+    end
+  end
+
+  defp traverse({:def, [line: line, column: _column], heads} = ast, issues, params, issue_meta) do
+    {ast, issues_for_function_definition(heads, line, issues, params, issue_meta)}
+  end
+
+  defp traverse({:defp, [line: line, column: _column], heads} = ast, issues, params, issue_meta) do
+    {ast, issues_for_function_definition(heads, line, issues, params, issue_meta)}
+  end
+
+  # Everything else passes through
+  defp traverse(ast, issues, _, _) do
+    {ast, issues}
+  end
+
+  defp walk({:|>, [line: _, column: _], args}, acc) do
+    Enum.reduce(args, acc, &walk/2)
+  end
+
+  defp walk({{:., metadata, [{:__aliases__, _, module}, :new]}, _, body}, acc) do
+    acc =
+      module
+      |> Enum.any?(&(&1 == :Multi))
+      |> case do
+        true -> Map.put(acc, :multi_new, metadata)
+        false -> acc
+      end
+
+    Enum.reduce(body, acc, &walk/2)
+  end
+
+  defp walk({{:., metadata, [{:__aliases__, _, module}, :transaction]}, _, body}, acc) do
+    acc =
+      module
+      |> Enum.any?(&(&1 == :Repo or &1 == :repo))
+      |> case do
+        true -> Map.put(acc, :repo_transaction, metadata)
+        false -> acc
+      end
+
+    Enum.reduce(body, acc, &walk/2)
+  end
+
+  defp walk({{:., _, [{:__aliases__, _, _}, _]}, _, body}, acc),
+    do: Enum.reduce(body, acc, &walk/2)
+
+  defp walk({:case, _, [[do: clauses]]}, acc) do
+    Enum.reduce(clauses, acc, &look_at_error_handling/2)
+  end
+
+  # trial and error is probably not going to scale ._.
+  defp walk(
+         {:case, _,
+          [
+            _,
+            [
+              do: [{:->, _, [_, {:__block__, _, [_, _, {:|>, _, clauses}]}]}, _]
+            ]
+          ]},
+         acc
+       ) do
+    Enum.reduce(clauses, acc, &walk/2)
+  end
+
+  defp walk({:case, _, [_, [do: [{:->, _, [_, clauses]}]]]}, acc) do
+    Enum.reduce(clauses, acc, &look_at_error_handling/2)
+  end
+
+  # NOTE: special exception for commented out code being checked in -- I have deleted this waaaaaay too many times now.
+  #
+  # defp walk(zut_alors, acc) do
+  #   dbg(zut_alors)
+  #   acc
+  # end
+
+  defp walk(_, acc), do: acc
+
+  # this is the happy case
+  defp look_at_error_handling(
+         {:->, _, [[{:{}, _, [:error, _, _, _]}], {:error, _} = two_tuple]},
+         acc
+       ) do
+    Map.put(acc, :error, two_tuple)
+  end
+
+  # good good, no concerns here
+  defp look_at_error_handling({:->, _, [[ok: _], _]}, acc), do: acc
+
+  # this area is concerning
+  defp look_at_error_handling({:->, _, [[{:=, _, _}], {:error, _, _}]}, acc), do: acc
+  defp look_at_error_handling({:->, _, [[{:{}, _, [:error, _, _]}], {:error, _}]}, acc), do: acc
+
+  # this area is noise
+  #
+  # would be better to scan and skip a method definition without Multi.new
+  defp look_at_error_handling({:->, _, _}, acc), do: acc
+
+  # This HAS to be improvable. It used to have more than one caller.
+  defp in_header?([{:__aliases__, _, _}, [do: {:__block__, [], header}]], section, module) do
+    header
+    |> Enum.filter(fn item ->
+      section == get_in(item, [Access.elem(0)])
+    end)
+    |> Enum.map(fn item ->
+      item
+      |> get_in([Access.elem(2)])
+      |> get_in([Access.at(0)])
+      |> get_in([Access.elem(2)])
+      |> get_in([Access.at(0)])
+    end)
+    |> Enum.any?(&Kernel.==(&1, module))
+  end
+
+  defp in_header?(_, _, _), do: false
+
+  defp issues_for_function_definition([_, [do: body]], _line, issues, _, issue_meta) do
+    case walk(body, %{}) do
+      # happy path!
+      %{:multi_new => _, :repo_transaction => _, error: {:error, _}} ->
+        issues
+
+      # warning, unable to find a 4 -> 2 tuple mapping!
+      %{:multi_new => [line: line, column: _column], :repo_transaction => _} ->
+        [issue_for("potential error handling concern", line, issue_meta) | issues]
+
+      _ ->
+        issues
+    end
+  end
+
+  defp issue_for(trigger, line_no, issue_meta) do
+    format_issue(
+      issue_meta,
+      message: "Potential false negative on Multi error handling in an Oban job",
+      trigger: "@#{trigger}",
+      line_no: line_no
+    )
+  end
+end
