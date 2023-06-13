862c42cb967a62193a28df084b20671f74301da0
Seth Schroeder <seth.schroeder@gmail.com>
2023-06-13 09:35:01 -0400

    Getting ready for my first package on `hex`

--- mix.exs
+++ mix.exs
@@ -4,10 +4,14 @@ defmodule CredoCheckErrorHandlingEctoOban.MixProject do
   def project do
     [
       app: :credo_check_error_handling_ecto_oban,
-      version: "0.9.0",
+      deps: deps(),
+      description: description(),
       elixir: "~> 1.14",
+      name: "CredoCheckErrorHandlingEctoOban",
+      package: package(),
+      source_url: "https://github.com/seth-schroeder/credo_check_error_handling_ecto_oban",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      version: "0.9.1",
     ]
   end
 
@@ -21,7 +25,27 @@ defmodule CredoCheckErrorHandlingEctoOban.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+      # maybe some day
+      # {:makeup_org, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    """
+    This is a custom Credo check that looks for a very specific edge case:
+
+    * Ecto.Repo.transaction/2 will return a 4 tuple when an error occurs inside a Multi.
+    * An Oban worker that returns an error 4 tuple will be considered a success.
+
+    """
+  end
+
+  defp package() do
+    [
+      licenses: ["GPL-3.0-or-later"],
+      links: %{"GitHub" => "https://github.com/seth-schroeder/credo_check_error_handling_ecto_oban"}
     ]
   end
 end
