a485b4b341e3a790384abc24f3b2efc603711afb
Seth Schroeder <seth.schroeder@gmail.com>
2023-06-13 08:54:23 -0400

    Licensing is important

--- lib/credo_check_error_handling_ecto_oban/check/transaction_error_in_oban_job.ex
+++ lib/credo_check_error_handling_ecto_oban/check/transaction_error_in_oban_job.ex
@@ -1,3 +1,11 @@
+# This file is part of credo_check_error_handling_ecto_oban.
+#
+# credo_check_error_handling_ecto_oban is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# credo_check_error_handling_ecto_oban is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 defmodule CredoCheckErrorHandlingEctoOban.Check.TransactionErrorInObanJob do
   @moduledoc """
   `Ecto.Repo.transaction/2` will return a 4 tuple when an error occurs inside a Multi, per
